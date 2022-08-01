2debc77c4b3c3b7b63871157562e2771167e7eb8
Claudia <claudia.araya.ro@gmail.com>
2022-08-01 16:59:41 -0400

    agregando texto

--- nasa_api.rb
+++ nasa_api.rb
@@ -22,7 +22,7 @@ def  build_web_page(data)
     # puts images
 
     cameras = "<!DOCTYPE html>\n<html lang='en'>\n<head>\n\t<meta charset=UTF-8>\n\t<meta http-equiv='X-UA-Compatible' content='IE=edge'>\n\t<meta name='viewport' content='width=device-width, initial-scale=1.0'>\n\t<title>Prueba Nasa</title>\n\t<meta name='author' content='Claudia Araya'>\n</head>\n" 
-    cameras += "\t<body style='background-color:#000000'>\n\t\t\t\t\t\t<h1 style='margin-left:4rem; color:#fefefe' > Imágenes del rover Curiosity</h1>\n\t\t\t<ul>"
+    cameras += "\t<body style='background-color:#000000'>\n\t\t\t\t\t\t<h1 style='margin-left:4rem; color:#fefefe' > Imágenes del rover Curiosity</h1>\n\t\t\t<h3 style='margin-left:4rem; color:#fefefe'>Nasa API</h3>\n\t\t\t<ul>"
 
     images.each do |image|
         cameras += "\n\t\t\t\t<li><img\tsrc='#{image}' style='width:25vw; margin-bottom:2rem'></li>"
