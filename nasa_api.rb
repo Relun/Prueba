90c0631d6c7b1d2128703615d4de2b7701f4a0b4
Claudia <claudia.araya.ro@gmail.com>
2022-08-01 11:05:21 -0400

    algunos cambios en diseño

--- nasa_api.rb
+++ nasa_api.rb
@@ -17,15 +17,15 @@ end
 
 nasa_info =  request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10")
 
-def  buid_web_page(data)
+def  build_web_page(data)
     images = data["photos"].map{|e| e['img_src']}
     # puts images
-    cameras = "<!DOCTYPE html>\n<html lang='en'>\n\t<head>\n\t</head>\n\t<body>\n\t\t<ul>"
+    cameras = "<!DOCTYPE html>\n<html lang='en'>\n\t<head>\n\t</head>\n\t<body>\n\t\t<h1>Nasa Api</h1>\n\t\t\t<ul>"
     images.each do |image|
-        cameras += "\n\t\t\t<li><img\tsrc='#{image}'></li>"
+        cameras += "\n\t\t\t\t<li><img\tsrc='#{image}'></li>"
     end
     cameras += "\n\t\t</ul>\n\t</body>\n</html>"
     File.write('nasaweb.html', cameras)
 end
-buid_web_page(nasa_info)
+build_web_page(nasa_info)
 
